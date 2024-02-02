ff98e176ba13a33e1094840741227236d1b2e2dd
JollyRogerRu <satortunov@yandex.ru>
2024-02-02 11:31:22 +0300

    Домашнее задание спринт 8 для сдачи. Вариант 3 с учетом замечаний Кнопка "Назад" реализована с помощью ImageView

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -4,14 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val button_settings = findViewById<Button>(R.id.asSettings)
-        button_settings.setOnClickListener {
+        val imageViewSettings = findViewById<ImageView>(R.id.asSettings)
+        imageViewSettings.setOnClickListener {
             finish()
         }
     }
