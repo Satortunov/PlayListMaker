eba7cb5d0f923da018bf9142321b2e0883f0ae41
JollyRogerRu <satortunov@yandex.ru>
2024-03-26 14:59:39 +0300

    Спринт 9, стили готовы, сделана отправка е-иейл, темы не до конца, мессенджеры не до конца. Отсальное не готово

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -11,9 +11,30 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val imageViewSettings = findViewById<ImageView>(R.id.asSettings)
+       val imageViewSettings = findViewById<ImageView>(R.id.asSettings)
         imageViewSettings.setOnClickListener {
-            finish()
+        finish()
+
+        val imageViewShare = findViewById<ImageView>(R.id.asShare)
+            imageViewShare.setOnClickListener {
+                val intentShare = Intent(Intent.ACTION_SEND);
+                intentShare.setType("text/plain");
+                val stringShare = "https://practicum.yandex.ru/android-developer/"
+                intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.share_subject));
+                intent.putExtra(Intent.EXTRA_TEXT, stringShare);
+                startActivity(Intent.createChooser(intent, getString(R.string.share_using)))
+            }
+
+
+        val imageViewSupport = findViewById<ImageView>(R.id.asSupport)
+        imageViewSupport.setOnClickListener {
+            val intentSupport = Intent(Intent.ACTION_SEND);
+            intentSupport.setType("text/html");
+            intentSupport.putExtra(Intent.EXTRA_EMAIL, "satortunov@yandex.ru");
+            intentSupport.putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker");
+            intentSupport.putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!");
+            startActivity(Intent.createChooser(intentSupport, "Send Email"))
         }
+
     }
 }
