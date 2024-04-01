9b327af59d81b366a016575f945f9073f060dd69
JollyRogerRu <satortunov@yandex.ru>
2024-04-01 13:35:15 +0300

    Сприинт 9 Работа с поиском. Реализована очистка поля поиска.

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -3,17 +3,29 @@ package com.example.playlistmaker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+
+        val linearLayout = findViewById<LinearLayout>(R.id.container)
+        val inputEditText = findViewById<EditText>(R.id.enterEditText)
+
+
+        inputEditText.setOnClickListener {
+            inputEditText.setHint("")
+            inputEditText.setCompoundDrawables(null, null, null, null)
+        }
+
+
         val imageViewSearch = findViewById<ImageView>(R.id.aseSearch)
         imageViewSearch.setOnClickListener {
             finish()
         }
-
     }
 }
