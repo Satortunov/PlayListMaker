8c053585a5b47785de44883095cab90ec6b8b7d0
JollyRogerRu <satortunov@yandex.ru>
2024-04-02 14:16:14 +0300

    Сприинт 9 Работа с поиском. Реализована очистка поля поиска и исчезновение кнопки

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,16 @@
 package com.example.playlistmaker
 
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,17 +20,27 @@ class SearchActivity : AppCompatActivity() {
 
         val linearLayout = findViewById<LinearLayout>(R.id.container)
         val inputEditText = findViewById<EditText>(R.id.enterEditText)
-
+        val searchImage = findViewById<ImageView>(R.id.search_Icon)
 
         inputEditText.setOnClickListener {
             inputEditText.setHint("")
-            inputEditText.setCompoundDrawables(null, null, null, null)
+
         }
-
-
+        searchImage.setOnClickListener {
+            inputEditText.setText("")
+            searchImage.visibility = searchImageVisibility("")
+        }
         val imageViewSearch = findViewById<ImageView>(R.id.aseSearch)
         imageViewSearch.setOnClickListener {
             finish()
+        }
+    }
+
+    private fun searchImageVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
         }
     }
 }
