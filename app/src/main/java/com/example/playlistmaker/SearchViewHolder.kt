98b2d64ba31e3d843b6b34e8a94a0af26208ffee
JollyRogerRu <satortunov@yandex.ru>
2024-06-21 14:33:58 +0300

    Спринт 10. Домашнее задание 2

--- app/src/main/java/com/example/playlistmaker/SearchViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/SearchViewHolder.kt
@@ -1,5 +1,8 @@
 package com.example.playlistmaker
 
+
+import android.content.Context
+import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -7,22 +10,29 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
+
 class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackName = itemView.findViewById<TextView>(R.id.trackName)
     private val artistName = itemView.findViewById<TextView>(R.id.artistName)
     private val trackTime = itemView.findViewById<TextView>(R.id.trackTime)
     private val artworkUrl = itemView.findViewById<ImageView>(R.id.artworkUrl)
 
+
     fun bind(item: Track) {
         trackName.text = item.trackName
         artistName.text = item.artistName
         trackTime.text = item.trackTime
 
+        fun Context.dpToPx(dp: Int): Float = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp.toFloat(),
+            resources.displayMetrics)
+
         Glide.with(itemView)
             .load(item.artworkUrl100)
             .placeholder(R.drawable.placeholder)
             .centerCrop()
-            .transform(RoundedCorners(2))
+            .transform(RoundedCorners(itemView.context.dpToPx(itemView.resources.getDimensionPixelSize(R.dimen.size_dp_2)).toInt()))
             .into(artworkUrl)
 
 
