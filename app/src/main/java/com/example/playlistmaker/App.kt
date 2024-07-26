680312795af8327b384bf43a2b2823ffb11d6e68
Satortunov <satortunov@yandex.ru>
2024-07-26 08:44:45 +0300

    Спринт 12. Домашняя работа

---
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -0,0 +1,43 @@
+package com.example.playlistmaker
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import android.content.SharedPreferences
+
+const val PLM_PREFERENCES = "playlist_maker_preferences"
+const val THEME_KEY = "theme_key"
+
+class App : Application() {
+
+    var darkTheme = false
+    private lateinit var sharedPreferences : SharedPreferences
+
+
+    override fun onCreate() {
+
+        sharedPreferences = getSharedPreferences(PLM_PREFERENCES,MODE_PRIVATE)
+        darkTheme = sharedPreferences.getBoolean(THEME_KEY,darkTheme)
+        super.onCreate()
+        switchTheme(darkTheme)
+        if (darkTheme == true) {
+        } else {  }
+    }
+
+    fun switchTheme(darkThemeEnabled: Boolean) {
+        darkTheme = darkThemeEnabled
+        AppCompatDelegate.setDefaultNightMode(
+            if (darkThemeEnabled) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+        sharedPreferences.edit().putBoolean(THEME_KEY, darkTheme).apply()
+    }
+
+    fun setThemeSwitch(darkThemeEnabled: Boolean){
+        getSharedPreferences(PLM_PREFERENCES, MODE_PRIVATE).edit()
+            .putBoolean(THEME_KEY, darkThemeEnabled)
+            .apply()
+    }
+}
